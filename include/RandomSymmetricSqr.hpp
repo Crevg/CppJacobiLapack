1b0c6f2f78d272a69fcdaf9e0bc0af30a6db68b7
Cristofer Villegas <zeldardar@hotmail.com>
2018-05-19 16:05:03 -0600

    init

---
+++ include/RandomSymmetricSqr.hpp
@@ -0,0 +1,29 @@
+#include <cstdlib>
+#include "Matrix.hpp"
+
+#ifndef ANPI_LU_HPP
+#define ANPI_LU_HPP
+
+namespace anpi
+{
+
+
+
+template<typename T>
+anpi::Matrix<T> randomSymmetricSqr(const size_t N){
+    anpi::Matrix<T> M = anpi::Matrix<T>(N, N, 0.0);
+    for (int i = 0; i < N; ++i){
+        
+        for (int k = 0; k < i; ++k){
+            M[i][k] = M[k][i];
+        }
+        for (int j = i; j < N ; ++j){
+            M[i][j] = (T) rand() * 100;
+        }
+    }
+    return M;
+}
+
+}//anpi
+
+#endif
