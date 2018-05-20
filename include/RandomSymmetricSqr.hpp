78b27a816e6bfe747218e708c351b73fddf4b3f6
Cristofer Villegas <zeldardar@hotmail.com>
2018-05-19 18:02:41 -0600

    Add jacobi basic rotation,  print matrix and change RSS

--- include/RandomSymmetricSqr.hpp
+++ include/RandomSymmetricSqr.hpp
@@ -1,8 +1,8 @@
 #include <cstdlib>
 #include "Matrix.hpp"
-
-#ifndef ANPI_LU_HPP
-#define ANPI_LU_HPP
+#include <time.h>
+#ifndef ANPI_RANDOM_SYMMETRIC_SQR_HPP
+#define ANPI_RANDOM_SYMMETRIC_SQR_HPP
 
 namespace anpi
 {
@@ -11,14 +11,17 @@ namespace anpi
 
 template<typename T>
 anpi::Matrix<T> randomSymmetricSqr(const size_t N){
+    srand(time(NULL));
+    T low = -1000;
+    T high = 1000;
     anpi::Matrix<T> M = anpi::Matrix<T>(N, N, 0.0);
-    for (int i = 0; i < N; ++i){
+    for (size_t i = 0; i < N; ++i){
         
-        for (int k = 0; k < i; ++k){
+        for (size_t k = 0; k < i; ++k){
             M[i][k] = M[k][i];
         }
-        for (int j = i; j < N ; ++j){
-            M[i][j] = (T) rand() * 100;
+        for (size_t j = i; j < N ; ++j){
+            M[i][j] = low + static_cast <T> (rand()) /(static_cast <T> (RAND_MAX/(high-low)));
         }
     }
     return M;
