52444d367ac1cf213772312e5a9fd047546ef8a3
e3r8ick <eguicoro2@gmail.com>
2018-05-19 21:33:29 -0600

    LapackFunctions

---
+++ include/LapackFunctions.hpp
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include "Matrix.hpp"
+
+#ifndef ANPI_LU_HPP
+#define ANPI_LU_HPP
+
+namespace anpi
+{
+
+template<typename T>
+void eig(const anpi::Matrix<T>& A,
+std::vector<T>& val ,
+anpi::Matrix<T>& E){
+    
+}
+
+}//anpi
+
+#endif
